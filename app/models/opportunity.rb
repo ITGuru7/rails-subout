52115e614a2323c5159011ada7b557e700ab9be0
Thomas Howe <howethomas@aol.com>
2012-08-05 19:25:55 -0400

    Updated opportunity

--- app/models/opportunity.rb
+++ app/models/opportunity.rb
@@ -13,4 +13,10 @@ class Opportunity
   field :buy_it_now, type: Boolean
   field :buy_it_now_price, type: BigDecimal 
   field :winning_bid_id, type: Integer
-end
+
+  belongs_to :company, :class_name => "Company", :foreign_key => "company_id"
+  has_one :opportunity_type, :class_name => "OpportunityType", :foreign_key => "opportunity_type_id"
+  has_one :winning_bid, :class_name => "Bid", :foreign_key => "winning_bid_id"
+  has_one :starting_location, :class_name => "Location", :foreign_key => "starting_location_id"
+  has_one :ending_location, :class_name => "Location", :foreign_key => "ending_location_id"
+en
