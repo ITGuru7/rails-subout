b9867a877b6a5c797c497f49acee46a1e6434ec2
coolsoftdev <suboutdev@gmail.com>
2012-08-14 05:01:55 +0300

    layout

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,130 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require jquery
-//= require jquery_ujs
-//= require_tree .
+//= require jquery.min
+//= require jquery.address-1.4
+  
+var mm_default = {
+	appendLoadingImg : function (selector) {
+		if($('.loading', selector).length==0)
+			$(selector).append('<div class="loading"><img class="loading_gif" src="/img/loading.gif" height="24" width="24" /></div>');
+	},
+	
+	removeLoadingImg : function () {
+		$('.loading').remove();
+	},
+	
+	init: function(){
+		/** global ajax setting **/
+		$.ajaxSetup({
+			beforeSend: function (jqXHR, settings) {
+				mm_default.appendLoadingImg($('#loading'));
+			},
+			complete: function(){
+				mm_default.removeLoadingImg();
+			}
+		});
+		
+		/** jquery address plugin setting **/
+		$('a.address').address(function() {  
+			return $(this).attr('href');  
+		});
+		
+		/** load login form **/
+		mm_user.buildLoginForm('#header_b');
+	},
+}
+
+var mm_user = {
+	auth_token : null,
+	login : function (email, password) {
+		//suboutdev@gmail.com sub0utd3v 
+		$.ajax({
+			type : "GET",
+			url : "/api_login.json",
+			data : {
+				'email' : email,
+				'password' : password
+			},
+			dataType : "json",
+			success : function (data) {
+				mm_user.auth_token = data.auth_token;
+				mm_nav.build('#header_b');
+			},
+			error : function (e) {
+				
+			}
+		});
+	},
+	buildLoginForm:function(container)
+	{
+		$.ajax({
+			type : "GET",
+			url : "/partial/loginform.html",
+			dataType : "html",
+			success : function (data) {
+				$(container).html(data);
+				mm_user.initLoginForm();
+			},
+			error : function (e) {
+				alert(e);
+			}
+		});
+	},
+	
+	initLoginForm: function()
+	{
+		$('#form_login').bind('submit', function(){
+			mm_user.login($('#input_email').val(), $('#input_password').val());
+			return false;
+		});	
+	}
+	
+}
+
+var mm_nav = {
+	items : {
+		'new_oppotunity' : {
+			href : '/new-oppotunity',
+			text : 'New Oppotunity',
+		},
+		'settings' : {
+			href : '/settings',
+			text : 'Settings',
+		},
+		'help' : {
+			href : '/help',
+			text : 'Help',
+		},
+		'signout' : {
+			href : '/signout',
+			text : 'Sign Out',
+		},
+	},
+	
+	build : function (container) {
+		
+		var nav = $('<nav></nav>');
+		$.each(mm_nav.items, function (id, item) {
+			$(nav).append($('<a>' + item.text + '</a>').attr('href', item.href).addClass('address'));
+		});
+		
+		$(':last', nav).addClass('last');
+		$(container).html(nav);	
+	}
+};
+
+
+$(function () {
+	mm_default.init();
+	
+
+	$.address.init(function(event){}).change(function(event) {
+		switch(event.path){
+			case '/new-oppotunity':
+			case '/settings':
+			case '/help':
+			case '/signout':
+		}
+	});
+})
