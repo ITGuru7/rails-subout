5a711502f9ab2613f99581a7ae43190cb85b3802
Gordon DuQuesnay <gduquesnay@gmail.com>
2012-09-19 23:13:27 +0900

    rename seller to supplier

--- features/a_buyer_adds_a_favorite.feature
+++ features/a_buyer_adds_a_favorite.feature
@@ -1,11 +1,9 @@
-Feature: A buyer adds a seller to their list of favorite suppliers
+Feature: A buyer adds a supplier to their list of favorite suppliers
 
-  Scenario: A buyer adds a seller as a favorite supplier
-    Given a seller exists called "Boston Bus"
+  Scenario: A buyer adds a member company as a favorite supplier
+    Given a supplier exists called "Boston Bus"
     And I am logged in as a buyer called "New York Bus"
-    When I add that seller as one of my favorite suppliers
-    Then that seller should be in my list of favorite suppliers
-  
-  Scenario: A buyer adds a guest company as a favorite supplier
+    When I add that supplier as one of my favorite suppliers
+    Then that supplier should be in my list of favorite suppliers
   
   Scenario: A buyer adds an unknown company as a favorite supplier
--- features/step_definitions/favorites_steps.rb
+++ features/step_definitions/favorites_steps.rb
@@ -1,5 +1,5 @@
-Given /^a seller exists called "(.*?)"$/ do |name|
-  @seller = FactoryGirl.create(:company, :name => name, :email => 'thomas@bostonbus.com')
+Given /^a supplier exists called "(.*?)"$/ do |name|
+  @supplier = FactoryGirl.create(:company, :name => name, :email => 'thomas@bostonbus.com')
 end
 
 Given /^I am logged in as a buyer called "(.*?)"$/ do |name|
@@ -8,15 +8,15 @@ Given /^I am logged in as a buyer called "(.*?)"$/ do |name|
   sign_in(buyer_user)
 end
 
-When /^I add that seller as one of my favorite suppliers$/ do
+When /^I add that supplier as one of my favorite suppliers$/ do
   click_on "Favorite suppliers"
-  fill_in "Email", :with => @seller.email
+  fill_in "Email", :with => @supplier.email
   click_on "Find Supplier"
-  page.should have_content @seller.name
+  page.should have_content @supplier.name
   click_on "Add to my favorite suppliers"
 end
 
-Then /^that seller should be in my list of favorite suppliers$/ do
+Then /^that supplier should be in my list of favorite suppliers$/ do
   click_on "Favorite supplier"
-  page.should have_content @seller.name
+  page.should have_content @supplier.name
 end
