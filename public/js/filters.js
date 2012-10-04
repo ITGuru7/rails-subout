9a49b4e27078d2c13a769613360508adcec3f7b7
coolsoftdev <thomashw2@gmail.com>
2012-10-05 03:16:15 +0800

    filter

--- public/js/filters.js
+++ public/js/filters.js
@@ -6,8 +6,12 @@ var Evaluators = {};
 Evaluators.range = function(input, evaluation)
 {
     var property = input[evaluation.property];
-    
-    return (property >= evaluation.params.min) && (property <= evaluation.params.max);
+    if(property)
+    {
+        return (property >= evaluation.params.min) && (property <= evaluation.params.max);
+    }else{
+        return true;
+    }
 }
 
 Evaluators.today = function(input, evaluation)
@@ -18,23 +22,33 @@ Evaluators.today = function(input, evaluation)
         var current_time = new Date().getTime();   
         return (parseInt(current_time/(3600 * 24 * 1000)) == parseInt(property/(3600 * 24 * 1000 )));
     }else{
-        return false;
+        return true;
     }
 }
 
 Evaluators.compare = function(input, evaluation)
 {
     var property = input[evaluation.property];
-    var compare = "'" + property + "'" + evaluation.params.operator + "'" + evaluation.params.value + "'";
-    return eval(compare);
+    if(property)
+    {
+        var compare = "'" + property + "'" + evaluation.params.operator + "'" + evaluation.params.value + "'";
+        return eval(compare);
+    }else{
+        
+        return true;
+    }
 }
 
 Evaluators.like = function(input, evaluation)
 {
     var property = input[evaluation.property];
-    
-    var reg = new RegExp(evaluation.params.value.toLowerCase());
-    return reg.test(property.toLowerCase());
+    if(property)
+    {
+        var reg = new RegExp(evaluation.params.value.toLowerCase());
+        return reg.test(property.toLowerCase());
+    }else{
+        return true;
+    }
 }
 
 function evaluation(input, evaluation){
@@ -45,7 +59,7 @@ function evaluation(input, evaluation){
         return evaluator(input, evaluation);
     }else{
         alert("Evaluator doesn't exist.");
-        return false;
+        return true;
     }
     
 }
