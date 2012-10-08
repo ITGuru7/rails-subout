fa34aa97b474c39a6f2579ad20071d8c84194483
Gordon DuQuesnay <gduquesnay@gmail.com>
2012-10-08 22:46:00 +0900

    win it now implemented as bid create

--- app/controllers/opportunities_controller.rb
+++ app/controllers/opportunities_controller.rb
@@ -7,9 +7,4 @@ class OpportunitiesController < ApplicationController
     #NOTE: thinking to use embed resource for bids
     @opportunity = Opportunity.find(params[:id])
   end
-
-  def win_it_now
-    @opportunity = Opportunity.find(params[:id])
-    @opportunity.quick_win_by!(current_company)
-  end
 end
