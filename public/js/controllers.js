7f70f61d212e6259ebdebed62b27bd325fc197ae
rubyjock <thomashw2@gmail.com>
2012-10-14 05:10:22 +0800

    tag

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -2,111 +2,129 @@
 
 /* Controllers */
 
-function AppController($scope, $location, $http, $filter, Token, Company){
-   if (($scope.user == undefined || $scope.user.authorized != 'true') && $location.path() != 'sign_in') {
-     $location.path('sign_in');
-   }
+function AppController($scope, $rootScope, $location, $http, $filter, Token, Company) {
+    if (($scope.user == undefined || $scope.user.authorized != 'true') && $location.path() != 'sign_in') {
+        $location.path('sign_in');
+    }
 
-    $scope.modal = '';
-    $scope.setModal = function(url)
-    {
-        $scope.modal = url;
+    $rootScope.setModal = function(url) {
+        $rootScope.modal = url;
     }
 }
 
-function OpportunityNewCtrl($scope, $location, Opportunity, Test, Filter, Tag, Company) {
-
-    $scope.opportunity = Opportunity.read({opportunityId: 'new'});
-
-
-    $scope.submit = function(){
-
-        var oppModel = new Opportunity();
-        oppModel.opportunity = $scope.opportunity;
-        oppModel.auth_token = $scope.token.auth_token;
-        oppModel.$create(function(data){
-            alert("New Opportunity was created.")
-        });
-
+function OpportunityNewCtrl($scope, $rootScope, $location, Opportunity, Test, Filter, Tag, Company) {
+    $scope.submit = function() {
+        var newOpportunity = $scope.opportunity;
+        Opportunity.save({opportunity:newOpportunity, api_token:$rootScope.user.api_token});
     }
 }
 
-function OpportunityCtrl($scope, $location, Opportunity) {
-     $scope.opportunities = Opportunity.query();
+function OpportunityCtrl($scope, $rootScope, $location, Opportunity) {
+    $scope.opportunities = Opportunity.query();
 }
 
-function DashboardCtrl($scope, $rootScope, $location, Opportunity, Filter, Tag, Company) {
-   $scope.opportunities = Opportunity.query({api_token: $rootScope.user.api_token});
-   //$scope.filters = Filter.query();
-   //$scope.tags = Tag.query();
+function DashboardCtrl($scope, $rootScope, $location, Opportunity, OpportunityTest, Filter, Tag, Company) {
+    $scope.opportunities = OpportunityTest.query({
+        api_token : $rootScope.user.api_token
+    });
+    
+    $scope.filters = Filter.query();
+    $scope.tags = Tag.query();
 
-   $scope.query = "";
-   $scope.filter = null;
+    $scope.query = "";
+    $scope.filter = null;
+    $scope.tag = null;
 
-
-   $scope.searchByFilter = function(input) {
-        if(!$scope.filter) return true;
+    $scope.searchByFilter = function(input) {
+        if (!$scope.filter)
+            return true;
         return evaluation(input, $scope.filter.evaluation);
-      };
+    };
 
-   $scope.searchByQuery = function(input) {
-        if(!$scope.query) return true;
+    $scope.searchByQuery = function(input) {
+        if (!$scope.query)
+            return true;
 
         var reg = new RegExp($scope.query.toLowerCase());
         return reg.test(input.name.toLowerCase()) || reg.test(input.seats);
-      };
+    };
 
-   $scope.searchByTag = function(input) {
+    $scope.searchByTag = function(input) {
+        if (!$scope.tag)
+            return true;
+            
+        var reg = new RegExp($scope.tag.name.toLowerCase());
+        if (reg.test(input.tags.toLowerCase())) {
 
-        var activeCount = 0;
-        for(var i=0; i<$scope.tags.length; i++){
+            return true;
+        }
+        return false;
+    };
 
-            if($scope.tags[i].active){
-                activeCount++;
-                var reg = new RegExp($scope.tags[i].name.toLowerCase());
-                if(reg.test(input.tags.toLowerCase())){
-
-                    continue;
-                }else{
-                    return false;
-                }
+    $scope.setFilter = function(filter) {
+        for (var i = 0; i < $scope.filters.length; i++) {
+            if($scope.filters[i]!=filter)
+            {
+                $scope.filters[i].active = false;
             }
         }
-        return true;
-      };
-
-   $scope.setFilter = function(filter){
-            for(var i=0; i<$scope.filters.length; i++)
-           {
-               $scope.filters[i].active = false;
-           }
-           filter.active = !filter.active;
-           $scope.filter = filter;
-           $scope.query = "";
+        filter.active = !filter.active;
+        if(filter.active)
+        {
+            $scope.filter = filter;
+        }else{
+            $scope.filter = null;
         }
+       
+        $scope.query = "";
+    }
 
-   $scope.setTag = function(tag){
-
-           tag.active = !tag.active;
+    $scope.setTag = function(tag) {
+        for (var i = 0; i < $scope.tags.length; i++) {
+            if($scope.tags[i]!=tag)
+            {
+                $scope.tags[i].active = false;
+            }
+            
         }
+        tag.active = !tag.active;
+        if(tag.active)
+        {
+            $scope.tag = tag;
+        }else{
+            $scope.tag = null;
+        }
+        
+        $scope.query = "";
+
+    }
 }
 
 function OpportunityDetailCtrl($scope, $routeParams, Opportunity) {
-  $scope.opportunity = Opportunity.get({opportunityId: $routeParams.opportunityId});
+    $scope.opportunity = Opportunity.get({
+        opportunityId : $routeParams.opportunityId
+    });
 }
 
 function SignInCtrl($scope, $rootScope, $location, Token, Company) {
-  $scope.signIn = function() {
-    Token.save({email:$scope.user.email, password:$scope.user.password}, function(user){
-      $rootScope.user = user;
-      if (user.authorized == "true") {
-        $scope.company = Company.get({companyId:user.company_id});
-        $location.path('dashboard');
-      }
-      else{
-        $scope.message = "Invalid username or password!"
-      }
-    });
-  }
+    $scope.email = "suboutdev@gmail.com";
+    $scope.password = "sub0utd3v";
+    $scope.signIn = function() {
+
+        Token.save({
+            email : $scope.email,
+            password : $scope.password
+        }, function(user) {
+            $rootScope.user = user;
+            if (user.authorized == "true") {
+                $rootScope.company = Company.get({
+                    companyId : user.company_id
+                });
+                $location.path('dashboard');
+            } else {
+                $scope.message = "Invalid username or password!"
+            }
+        });
+    }
 }
 
