69d6a74937d81704ba57fa2106423760d4c665bf
Gordon DuQuesnay <gduquesnay@gmail.com>
2012-10-15 23:42:36 +0900

    Mock data login for testing

--- app/controllers/api/v1/tokens_controller.rb
+++ app/controllers/api/v1/tokens_controller.rb
@@ -1,8 +1,8 @@
 class Api::V1::TokensController < Api::V1::BaseController
   def create
     logger.info params
-    user = User.where(:email => params[:email]).first
-    if user && user.valid_password?(params[:password])
+    user = User.where(:email => "suboutdev@gmail.com").first
+    if user && user.valid_password?("sub0utd3v")
       render :json => { api_token: user.authentication_token, authorized: 'true', company_id: user.company_id }
     else
       render :json => { authorized: 'false' }
