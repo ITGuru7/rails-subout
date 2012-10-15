3e64911646d9894c88b7906e8efb83a2bef901ab
Gordon DuQuesnay <gduquesnay@gmail.com>
2012-10-15 23:35:39 +0900

    log params

--- app/controllers/api/v1/tokens_controller.rb
+++ app/controllers/api/v1/tokens_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::TokensController < Api::V1::BaseController
   def create
+    logger.info params
     user = User.where(:email => params[:email]).first
     if user && user.valid_password?(params[:password])
       render :json => { api_token: user.authentication_token, authorized: 'true', company_id: user.company_id }
