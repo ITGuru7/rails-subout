c43ffddf90c4dbbb4f50f098be180bba66cd1be3
Hoon <ooleem@gmail.com>
2012-12-25 00:26:46 -0800

    [Fixes #41205035] Nothing happens when you click 'Win now'. https://www.pivotaltracker.com/story/show/41205035

--- app/serializers/opportunity_short_serializer.rb
+++ app/serializers/opportunity_short_serializer.rb
@@ -1,7 +1,7 @@
 class OpportunityShortSerializer < ActiveModel::Serializer
   attributes :_id, :created_at, :name, :icon, :type, :for_favorites_only,
     :quick_winnable, :bidable?, :buyer_id, :start_region, :end_region,
-    :buyer_name, :buyer_abbreviated_name, :reference_number, :canceled
+    :buyer_name, :buyer_abbreviated_name, :reference_number, :canceled, :win_it_now_price
 
   def icon
     "icon-#{opportunity_short.type.parameterize}"
