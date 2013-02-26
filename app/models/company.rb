228f8deb638adce5ff16c9e99d52932bedf6b060
Gordon DuQuesnay and Hoon Kang <suboutdev+gordon+hoon@gmail.com>
2013-02-26 06:53:07 +0000

    [Finishes #44773879] Show number format on sales info messages

--- app/models/company.rb
+++ app/models/company.rb
@@ -191,9 +191,9 @@ class Company
 
   def sales_info_messages
     sales_messages = []
-    sales_messages << "$#{total_sales} in sales" if total_sales > 0
-    sales_messages << "$#{total_winnings} in winnings" if total_winnings > 0
-    sales_messages << "Bid on #{opportunities_bid_on.size} opportunities worth $#{opportunities_bid_on.sum(&:value)}" if bids.count > 0
+    sales_messages << "#{ActionController::Base.helpers.number_to_currency(total_sales)} in sales" if total_sales > 0
+    sales_messages << "#{ActionController::Base.helpers.number_to_currency(total_winnings)} in winnings" if total_winnings > 0
+    sales_messages << "Bid on #{opportunities_bid_on.size} opportunities worth #{ActionController::Base.helpers.number_to_currency(opportunities_bid_on.sum(&:value))}" if bids.count > 0
 
     sales_messages << "No activity so far" if sales_messages.empty?
 
