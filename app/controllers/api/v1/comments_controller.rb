3c173d9b05aaba44fa89f6207bf91167fd77a366
Gordon DuQuesnay and Hoon Kang <suboutdev+gordon+hoon@gmail.com>
2013-02-28 08:25:14 +0000

    [Finishes #44726909, #45241863] Section to Ask a Question to the poster. Change the information displayed on company profile popup

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::CommentsController < Api::V1::BaseController
   def create
     opportunity = Opportunity.find(params[:opportunity_id])
+    raise "Access denied" unless opportunity.buyer_id == current_company.id
     comment = opportunity.comments.create(:body => params[:comment][:body], :commenter => current_company)
     respond_with_namespace(opportunity, comment)
   end
--- app/serializers/actor_serializer.rb
+++ app/serializers/actor_serializer.rb
@@ -1,5 +1,5 @@
 class ActorSerializer < ActiveModel::Serializer
-  attributes :_id, :name, :abbreviated_name, :logo_url
+  attributes :_id, :name, :abbreviated_name, :logo_url, :contact_name, :email
 
   def logo_url
     Cloudinary::Utils.cloudinary_url(object.logo_id, width: 180, crop: :scale, format: 'png')
