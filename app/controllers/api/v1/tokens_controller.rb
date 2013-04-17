0a3cbfd596743c3e1764308ade0c1e9b49ea37e4
Suboutdev <suboutdev@gmail.com>
2013-04-17 06:54:28 +0000

    [#47380075] Fix error message for locked account. Hide become user for locked account

--- app/controllers/api/v1/tokens_controller.rb
+++ app/controllers/api/v1/tokens_controller.rb
@@ -5,7 +5,7 @@ class Api::V1::TokensController < Api::V1::BaseController
     username = params[:email].downcase if params[:email]
     if user = User.where(:email => username).first
       if user.access_locked?
-        render :json => { authorized: false, message: "Your account is locked. Please contact to admin."}
+        render :json => { authorized: false, message: "Your account is locked. Please contact admin."}
       elsif user.valid_password?(params[:password])
         render json: user.auth_token_hash
         user.update_tracked_fields!(request)
