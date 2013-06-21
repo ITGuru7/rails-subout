ba0211f6fa3752d297ef86b4f88ddfc148d86f81
Suboutdev <suboutdev@gmail.com>
2013-06-21 08:28:00 +0000

    [Fixes #47848871] Add a additional feature that acknowledges the equipment on multiple buses jobs.

--- app/models/opportunity.rb
+++ app/models/opportunity.rb
@@ -178,7 +178,7 @@ class Opportunity
     Notifier.delay.won_auction_to_buyer(self.id) if self.buyer.notification_items.include?("opportunity-win")
     Notifier.delay.won_auction_to_supplier(self.id) if bid.bidder.notification_items.include?("opportunity-win")
     
-    if self.vehicle_count != bid.vehicle_count and self.bidding_ends_at > Time.now
+    if self.vehicle_count != bid.vehicle_count #and self.bidding_ends_at > Time.now
       new_opportunity_attrs = self.attributes.except("_id", "reference_number", "created_at", "upated_at", "bidding_won_at", "value", "winning_bid_id", "bidding_done", "favorites_notified", "bidding_ends_at")
       new_opportunity_attrs["vehicle_count"] = self.vehicle_count - bid.vehicle_count
       new_opportunity_attrs["reserve_amount"] = self.reserve_amount / self.vehicle_count * new_opportunity_attrs["vehicle_count"]
@@ -193,7 +193,7 @@ class Opportunity
             Notifier.delay.finished_auction_to_bidder(self.id, old_bid.bidder_id)
             next
           else
-            old_bid.amount = old_bid.amount.to_i / old_bid.vehicle_count * new_opportunity.vehicle_count
+            #old_bid.amount = old_bid.amount.to_i / old_bid.vehicle_count * new_opportunity.vehicle_count
             old_bid.vehicle_count = new_opportunity.vehicle_count 
           end
         end
