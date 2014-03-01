a0dcef2e297a73333175300ce415ea615d527cf6
Suboutdev <suboutdev@gmail.com>
2014-03-01 20:35:19 +0000

    Redirect to www.suboutapp.com from suboutapp.com

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_filter :check_uri
   before_filter :https_redirect
 
   private
+
+  def check_uri
+    redirect_to "https://www.suboutapp.com#{request.fullpath}" if "suboutapp.com" == request.host
+  end
 
   def https_redirect
     if ENV["ENABLE_HTTPS"]
