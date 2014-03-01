d01bef7e7c552a4273f89cb8614cdff6483d732e
Suboutdev <suboutdev@gmail.com>
2014-03-01 19:43:33 +0000

    Force SSL on subouttest

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+  before_filter :https_redirect
+
+  private
+
+  def https_redirect
+    if ENV["ENABLE_HTTPS"]
+      if !request.ssl?
+        flash.keep
+        redirect_to protocol: "https://", status: :moved_permanently
+      end
+    end
+  end
 end
