b6c232669a46c50334ffda0c0820530297de189a
Suboutdev <suboutdev@gmail.com>
2014-03-24 14:14:13 +0000

    added a validation to Setting model

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -4,8 +4,11 @@ class Setting
 
   field :key
   field :value
+
+  Setting::KEYS = ["admin_email", "application_message", "marketing_message"]
   
   validates_presence_of :value
+  validate :validate_key
   validate :validate_santinized_length, if: :application_message? 
 
   def to_param
@@ -26,6 +29,10 @@ class Setting
 
   def application_message?
     self.key == "application_message"
+  end
+
+  def validate_key
+    Setting::KEYS.include?(key)
   end
 
   def validate_santinized_length
