784778e7145886c4e99aedb9cfdc8403efd77c29
Suboutdev <suboutdev@gmail.com>
2014-04-05 00:53:06 +0000

    [ghost user] skip before filter for update_agreement

--- app/controllers/api/v1/companies_controller.rb
+++ app/controllers/api/v1/companies_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::CompaniesController < Api::V1::BaseController
   skip_before_filter :restrict_access, only: :create
-  skip_before_filter :restrict_ghost_user, only: :create
+  skip_before_filter :restrict_ghost_user, only: [:create, :update_agreement]
   serialization_scope :current_company
 
   def index
