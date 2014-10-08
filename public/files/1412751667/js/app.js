d28507b7fa7d5ab2a4ad71cdfb8a0227883d91d7
Suboutdev <suboutdev@gmail.com>
2014-10-08 07:04:01 +0000

    Run cache bursting

--- public/files/1412751667/js/app.js
+++ public/files/1412751667/js/app.js
@@ -151,6 +151,7 @@ var AvailableOpportunityCtrl, BidNewCtrl, CompanyDetailCtrl, CompanyProfileCtrl,
 
 subout.run(function($rootScope, $location, $appBrowser, $numberFormatter, $timeout, Opportunity, Company, Favorite, User, FileUploaderSignature, AuthToken, Region, Bid, Setting) {
   var REGION_NAMES, d, p, salt, _i, _ref, _results;
+  $rootScope._ = _;
   $rootScope.stars = [1, 2, 3, 4, 5];
   d = new Date();
   $rootScope.years = (function() {
@@ -692,11 +693,20 @@ NegotiationNewCtrl = function($scope, $rootScope, Bid, Opportunity, MyBid, Aucti
 };
 
 BidNewCtrl = function($scope, $rootScope, Bid, Opportunity) {
+  var _base;
   if (!$scope.bid) {
     $scope.bid = {};
   }
   $scope.bid.vehicle_count = $scope.opportunity.vehicle_count;
+  (_base = $scope.bid).vehicle_count || (_base.vehicle_count = 1);
   $scope.bid.amount = Opportunity.defaultBidAmountFor($scope.opportunity);
+  $scope.bid.vehicles = [];
+  $scope.$watch("bid.vehicle_count", function() {
+    $scope.bid.vehicles = [];
+    return _($scope.bid.vehicle_count).times(function() {
+      return $scope.bid.vehicles.push({});
+    });
+  });
   $scope.hideAlert = function() {
     return $scope.errors = null;
   };
@@ -1932,7 +1942,7 @@ subout.directive('multiple', function() {
         buttonText: function(options, select) {
           var selected;
           if (options.length === 0) {
-            return 'All regions <b class="caret"></b>';
+            return "" + attrs.placeholder + " <b class='caret'></b>";
           } else if (options.length > 3) {
             return options.length + ' selected <b class="caret"></b>';
           } else {
@@ -2050,6 +2060,19 @@ module.filter("websiteUrl", function() {
       return url;
     } else {
       return "http://" + url;
+    }
+  };
+});
+
+module.filter("excerpt", function() {
+  return function(input, limit) {
+    if (!input) {
+      return "";
+    }
+    if (input.length > limit) {
+      return input.substring(0, limit) + "...";
+    } else {
+      return input;
     }
   };
 });
