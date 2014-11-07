bfc65632b994315c0dcefdadc96274cd6b61d2c7
Suboutdev <suboutdev@gmail.com>
2014-11-07 06:23:37 +0000

    speed test

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -1,16 +1,14 @@
 class Api::V1::EventsController < Api::V1::BaseController
   def index
-    events = Event.includes([:actor, :eventable]).recent.for(current_company)
+    events = Event.includes([:eventable]).recent.page(params[:page]).for(current_company)
     events = events.where(:"action.type" => params[:event_type]) if params[:event_type]
 
     regions = params[:regions].split(',') unless params[:regions].blank?
-
     events = events.where(:regions.in => regions) unless regions.blank?
+
     events = events.where(:cached_eventable_type => params[:opportunity_type]) if params[:opportunity_type]
     events = events.where(:actor_id => params[:company_id]) if params[:company_id]
     events = events.search(params[:q]) if params[:q]
-    events = events.page(params[:page])
-
     render json: events
   end
 end
