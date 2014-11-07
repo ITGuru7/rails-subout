19fa0cba2f647aef8af169e2ede9fa7b71daad96
Suboutdev <suboutdev@gmail.com>
2014-11-07 06:33:39 +0000

    removed description from opportunity short serializer

--- app/serializers/opportunity_short_serializer.rb
+++ app/serializers/opportunity_short_serializer.rb
@@ -2,7 +2,7 @@ class OpportunityShortSerializer < ActiveModel::Serializer
   attributes :_id, :created_at, :name, :icon, :type, :for_favorites_only,
     :quick_winnable, :bidable?, :buyer_id, :start_region, :end_region, :vehicle_type, :trip_type,
     :buyer_name, :buyer_abbreviated_name, :reference_number, :canceled, :in_negotiation, :win_it_now_price, :status,
-    :bidding_ends_at, :description, :ada_required, :start_date, :reserve_amount, :forward_auction, :vehicle_count, 
+    :bidding_ends_at, :ada_required, :start_date, :reserve_amount, :forward_auction, :vehicle_count, 
     :special_region, :is_for_special_region
 
   def icon
