7344ca73dbee94da180abf86267a41d1edd15803
Suboutdev <suboutdev@gmail.com>
2014-11-07 06:03:50 +0000

    speed issue - serializer changes

--- app/serializers/event_serializer.rb
+++ app/serializers/event_serializer.rb
@@ -1,7 +1,12 @@
 class EventSerializer < ActiveModel::Serializer
-  attributes :_id, :created_at, :regions, :eventable_company_id
+  attributes :_id, :created_at, :regions, :eventable_company_id, :actor
 
-  has_one :actor, serializer: ActorSerializer
+  #for the speeding
+  #has_one :actor, serializer: ActorShortSerializer
   has_one :action
   has_one :eventable, serializer: OpportunityShortSerializer
+
+  def actor
+    {:_id => object.actor_id}
+  end
 end
